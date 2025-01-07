eaed4e5d2380fa29c77038de7149983c4c9993bc
Sai2k6 <121502464+Sai2k6@users.noreply.github.com>
2025-01-07 13:25:14 +0530

    Add files via upload

---
+++ Task-02/task 2.py
@@ -0,0 +1,31 @@
+from PIL import Image
+import pytesseract
+
+
+image_path = "/home/sair/Pictures/Screenshots/Screenshot_20250107_132217.png"
+image = Image.open(image_path)
+
+
+extracted_text = pytesseract.image_to_string(image)
+
+extracted_text = extracted_text.strip()
+print(f"Extracted Text: {extracted_text}")
+extracted_text = extracted_text.replace(" ", "")
+extracted_text = extracted_text.replace("÷", "/")
+extracted_text = extracted_text.replace("×", "*")
+extracted_text = extracted_text.replace("x", "*")
+extracted_text = extracted_text.replace("—", "-")
+extracted_text = extracted_text.replace("^", "**")
+
+
+is_valid = 1
+for char in extracted_text:
+    if not (char.isdigit() or char in "+-*/().×÷—^ "): 
+        is_valid = 0
+        break
+
+if is_valid:
+    result = eval(extracted_text)
+    print(f"{extracted_text} = {result}")
+else:
+    print("The extracted text is not a valid arithmetic expression.")
